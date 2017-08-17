4fd9d372a0054712a2ceb3b2d410d88d5a99b54e
toby-adedipe <toby_adedipe@yahoo.com>
2017-08-17 23:45:41 +0100

    added blog and about me page

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def index
     @post = Post.all
   end
+
+  def about
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#index"
   resources :posts
-  post 'posts/save'
+  get 'blog', to: 'blogs#index'
+  get 'about', to: 'pages#about'
 end
