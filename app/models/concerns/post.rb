e80c3165456139d7a7d4c8aa9e6f2df634efabe4
toby-adedipe <toby_adedipe@yahoo.com>
2017-08-22 23:11:02 +0100

    few corections

--- app/models/concerns/post.rb
+++ app/models/concerns/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :user
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
 end
