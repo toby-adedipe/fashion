8a72df088c56bd20671df40dc95d495608f8805e
toby-adedipe <toby_adedipe@yahoo.com>
2017-08-24 00:20:11 +0100

    added comments functionality

--- app/models/concerns/post.rb
+++ app/models/concerns/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "pages#index"
-  resources :posts
+
+  resources :posts do
+    resources :comments
+  end
+
   get 'blog', to: 'blogs#index'
   get 'about', to: 'pages#about'
 end
